778cdbe4ee43e4d176b256d473a41d675618b7eb
CHOIGOYO <choigoyo.q7@gmail.com>
2023-06-04 15:14:46 +0900

    페이지 디자인- 커스텀 위젯에서 App Bar의 설정으로 배경색, 중앙정렬, 앱바의 높이 지정을 해주었고, 바디 부분에서 텍스트 2개의 설정으로 글씨색상과 크기 그리고 간격 굵기를 설정했다.
    SizedBox를 이용해서 두 텍스트 사이의 간격을 조절하였고
    박스의 속정으로는 높이와 가로길이를 지정해줄 수 있다.
    
    바디부분의 패딩값을 지정해주었고
    텍스트의 가로축 정렬로 crossAxisAlignment을 사용했다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,53 @@
-import 'package:flutter/material.dart'; // 플러터 프레임워크 기본 위젯 디자인 테마 요소를 사용하기 위해 import
+import 'package:flutter/material.dart';
 
-// app 의 시작점
-void main() => runApp(MyApp());
-// 최상위 위젯이 될 MyApp Stateless Widgets
+void main()=> runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First App', // 앱을 총칭하는 이름
-      theme: ThemeData( // ThemeData - 기본적인 디자인 테마 지정
-        primarySwatch:  Colors.blue // primarySwatch 색상견본
-      ),
-    home: MyHomePage() , // app이 정상적으로 실행되었을 때 가장 먼저 보여주는 경로  MyHomePage은 커스텀 위젯
+      title: 'BBANTO',
+      home: Grade(),
     );
   }
 }
 
-
-// 커스텀 위젯을 만들때 stateless or stateful 위젯으로 만들지 먼저 생각해야한다.
-// 첫번째 실습으로 간단한 문자출력 앱을 만들것 -> Stateless 위젯으로 실습
-class MyHomePage extends StatelessWidget{
+class Grade extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: Text('My App'), // App Bar의 표시되는 타이틀
-        centerTitle: true, // 타이틀 중앙 정렬
-        backgroundColor: Colors.redAccent, // App Bar의 색상 지정
-        elevation: 0.0, // App Bar 높이 지정(그림자 사라짐)
+        title: Text('BBANTO'),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true,
+        elevation: 0.0,
       ),
-      body: Center( // 컬럼 위젯을 가로축으로 정렬 하려면 Center 위젯이 필요하고, 세로축으로 정렬 하려면 mainAxisAlignment 이 필요하다.
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center, // 컬럼은 세로 정렬만 하지만 mainAxisAlignment 은 앱 스크린 내에서 세로축으로 상단 하단등을 정렬할 때 쓰인다.
-            children: [
-              Text('Hello'),
-              Text('Hello'),
-              Text('Hello'),
-            ],
-          ),
+          crossAxisAlignment: CrossAxisAlignment.start, // 텍스트의 가로축 정렬 CrossAxisAlignment
+          children: [
+            Text('NAME',
+            style: TextStyle(
+              color: Colors.white, // 글자 색상
+              letterSpacing: 2.0, // 글자 간격
+            ) ,
+            ),
+            SizedBox( // 보이지않지만, 두 텍스트 사이에 간격을 조절할 수 있음
+              height: 10.0 , // 박스의 높이
+              // width: , 박스의 가로 길이
+            ),
+            Text('BBANTO',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0, // 글씨 사이즈
+                fontWeight: FontWeight.bold // 글씨 굵기
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
