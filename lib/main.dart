83bd4c4150c3f7692060143db11094cabaab23ee
CHOIGOYO <choigoyo.q7@gmail.com>
2023-06-04 17:41:24 +0900

    pubspec.yml 파일속성에서 사용할 이미지의 경로를 넣어주고 gif, png 파일을 추가했다. CircleAvatar을 사용해서 사용할 이미지의 경로와 사이즈 등 설정을 지정해줄 수 있다. Divider 을 사용해서 구분선을 사용했다.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BBANTO',
+      debugShowCheckedModeBanner: false,
+      title: '춘식이',
       home: Grade(),
     );
   }
@@ -18,7 +19,7 @@ class Grade extends StatelessWidget{
     return Scaffold(
       backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: Text('BBANTO'),
+        title: Text('춘식이'),
         backgroundColor: Colors.amber[700],
         centerTitle: true,
         elevation: 0.0,
@@ -28,6 +29,18 @@ class Grade extends StatelessWidget{
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start, // 텍스트의 가로축 정렬 CrossAxisAlignment
           children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/cc.gif'),
+                radius: 60.0,
+              ),
+            ),
+            Divider( // 구분선
+              height: 60.0, // 구분선으로부터 위 간격 30 아래 간격 30
+              color: Colors.grey[850],
+              thickness: 0.5,// 선의 두께
+              endIndent: 30.0,
+            ),
             Text('NAME',
             style: TextStyle(
               color: Colors.white, // 글자 색상
@@ -38,14 +51,85 @@ class Grade extends StatelessWidget{
               height: 10.0 , // 박스의 높이
               // width: , 박스의 가로 길이
             ),
-            Text('BBANTO',
+            Text('춘식이',
               style: TextStyle(
                 color: Colors.white,
                 letterSpacing: 2.0,
                 fontSize: 28.0, // 글씨 사이즈
                 fontWeight: FontWeight.bold // 글씨 굵기
               ),
-            )
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text('춘식이 POWER LEVEL',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ) ,
+            ),
+            SizedBox(
+              height: 10.0 ,
+            ),
+            Text('14',
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('using lightsaber',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0,
+                ),
+                )
+              ],
+            ),Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('face hero tattoo',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('fire flames',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/cc2.png'),
+                backgroundColor: Colors.amber[800],
+                radius: 60.0, // 이미지의 크기
+              ),
+            ),
           ],
         ),
       ),
