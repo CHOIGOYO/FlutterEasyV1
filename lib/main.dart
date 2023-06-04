4ff3583254bfabad7c7227b5e57c939b810778a3
CHOIGOYO <choigoyo.q7@gmail.com>
2023-06-04 13:20:16 +0900

    flutter는 모두 위젯으로 되어있으며 main함수로 실행된다. 최상위 위젯이되는 MyApp은 정적인 위젯으로 Stateless 위젯을 상속받아 MaterialApp을 리턴한다. MaterialApp앱에서는 앱을 총칭하는 이름과 기본적인 테마를 지정해주고 home 에서 앱이 정상적으로 실행되었을 때 보여질 커스텀위젯을 호출해준다.
    커스텀 위젯을 작성하기 전, 동적인위젯인지 정적인위젯인지 먼저 생각하고 이를 상속받는다.

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,34 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First App',
+      title: 'First App', // 앱을 총칭하는 이름
       theme: ThemeData( // ThemeData - 기본적인 디자인 테마 지정
         primarySwatch:  Colors.blue // primarySwatch 색상견본
       ),
-    home: MyHomePage() , // app이 정상적으로 실행되었을 때 가장 먼저 보여주는 경로
+    home: MyHomePage() , // app이 정상적으로 실행되었을 때 가장 먼저 보여주는 경로  MyHomePage은 커스텀 위젯
     );
   }
 }
 
+
+// 커스텀 위젯을 만들때 stateless or stateful 위젯으로 만들지 먼저 생각해야한다.
+// 첫번째 실습으로 간단한 문자출력 앱을 만들것 -> Stateless 위젯으로 실습
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First App'), // 앱 내에 appbar의 타이틀로 보여지는 텍스트
+      ),
+      body: Center( // 중앙 배치
+        child: Column( // 세로 배치
+          children: [ // [] 내에 세로로 배치될 내용을 넣어주면 된다.
+            Text('hello'), // 위젯 간 구분을 위해 쉼표를 넣어줘야한다.
+            Text('hello'),
+            Text('hello')
+          ],
+        ),
+      ),
+    );
+  }
+}
