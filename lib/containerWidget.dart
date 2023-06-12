99a83404851b4a23c2e6ffca2a0b18dde2d2486c
CHOIGOYO <choigoyo.q7@gmail.com>
2023-06-12 18:03:09 +0900

    SafeArea 위젯을 사용하면 화면 밖으로 나가지 않도록 경계를 지정해줄 수 있다. Container는 오직 하나의 child를 갖는다 margin은 위젯의 밖을 padding은 위젯의 안쪽을 조절할 수 있다.

--- lib/containerWidget.dart
+++ lib/containerWidget.dart
@@ -20,9 +20,19 @@ class MyPage123 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
-      body: Container(
-        color: Colors.red, // containers with no children try to be as big as possible
-        child: Text('hello'), //
+      body: SafeArea( // 화면에 맞춤
+        child: Container(
+          color: Colors.red, // containers with no children try to be as big as possible
+          width: 100, // 컨테이너 내 크기를 지정
+          height: 100,
+          // margin: EdgeInsets.all(20), // 마진 값을 상하좌우 20씩 줌
+          margin: EdgeInsets.symmetric( // x,y 값을 주어 마진을 준다
+            vertical: 80,
+            horizontal: 20
+          ),
+          padding: EdgeInsets.all(25),
+          child: Text('hello'), // 컨테이너는 자식을 가지게되면 크기가 자식 사이즈로 작아진다.
+        ),
       ),
     );
   }
